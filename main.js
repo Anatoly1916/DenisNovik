a0bac557c2aadfe90732bec00fdab2da917ffcb0
Anatoly1916 <dogsinspace1916@gmail.com>
2023-02-26 12:45:34 +0300

    fix: fix js file

--- main.js
+++ main.js
@@ -2,27 +2,38 @@ const burger = document.querySelector('.burger');
 const line = document.querySelectorAll('.line');
 const popUp = document.querySelector('.hide');
 const inner = document.querySelector('.inner');
-const list = document.querySelectorAll('.header__list');
+const list = document.querySelector('.header__list');
+const nav = document.querySelectorAll('.header__link');
 const lang = document.querySelector('.lang');
+const ruEng = document.querySelector('.ru-eng');
+const hideBox = document.querySelector('.hide__lang');
+
 
 burger.addEventListener('click', (e) => {
-    line.forEach(item => 
-        item.classList.toggle('turned'));
-        popUp.classList.toggle('visible');
-        renderPopup();
+    line.forEach((item) => item.classList.toggle('turned')); 
+    
+    for(let i = 0; i < line.length; i++) {
+        if(line[i].classList.contains('turned')) {
+           renderPopup();
+        }else {
+            closeNav();
+        }
+    }
 });
 
 function renderPopup() {
     lang.style.display = 'block';
-    list.forEach((item) =>
+    nav.forEach((item) =>
      inner.appendChild(item));
-     inner.after(lang);
-     
+     hideBox.appendChild(ruEng);
+     popUp.classList.toggle('visible');
 }
 
 function closeNav() {
     popUp.classList.remove('visible');
+    nav.forEach((item) => list.appendChild(item));
     line.forEach((item) => item.classList.remove('turned'));
+    lang.append(ruEng);
 
 }
-list.forEach((elem) => elem.addEventListener('click', closeNav));
+nav.forEach((elem) => elem.addEventListener('click', closeNav));
